83c804c332f56406a69f75682109cce016226f10
Jonas Wahringer <jonas.wahringer@gmail.com>
2026-05-26 00:47:01 +0200

    Remove useless sync/async/stream cache interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { AsyncCache, CacheType, ICache, StreamCache, SyncCache } from "./defs/cache";
+export type { CacheValue, ICache } from "./defs/cache";
 export { TTL } from "./defs/options";
 export type { Options, Retention, Scope } from "./defs/options";
 export { CacheFactory } from "./generate";
